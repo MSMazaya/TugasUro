964d12e3c33532e7fc56cf65ab73f2044b01c1fa
MazayaCode <msulthanmazaya@gmail.com>
2021-03-28 17:15:58 +0700

    init project

--- Tubes.cpp
+++ Tubes.cpp
@@ -3,48 +3,30 @@
 #include<algorithm>
 #include <time.h>
 #include <stdlib.h> 
+#include<cmath>
 using namespace std;
 
 class Robot{
     public:
         pair<int,int> coordinate;
-        pair<int,int> boundary;
-        void init(pair<int,int> mapsize){ 
+        long long panjang_senjata;
+        void init(int senjata){ 
             coordinate=make_pair(0,0);
-            boundary=mapsize;
-        }
-        string gerak(string gerak){
-            int *x = &coordinate.first;
-            int *y = &coordinate.second;
-            if(gerak == "kanan"){
-                if(*x == boundary.first-1) return "Melebihi map\n";
-                *x += 1; 
-            } else if(gerak == "kiri") {
-                if(*x == 0) return "Melebihi map\n";
-                *x -= 1;
-            } else if(gerak=="atas") {
-                if(*y == boundary.second-1) return "Melebihi map\n";
-                *y += 1;
-            } else if(gerak=="bawah"){
-                if(*y == 0) return "Melebihi map\n";
-                *y -= 1;
-            } else {
-                return "Input tidak valid!\n";
-            }
-            return "";
+            panjang_senjata = senjata;
         }
 };
 
 class Kecoa{
     public:
         pair<int,int> coordinate;
-        void init(pair<int,int> mapsize){
+        pair<int,int> 
+        init(pair<int,int> mapsize){
             srand ( time(NULL) );
             while(coordinate == make_pair(0,0)){
                 coordinate.first = rand() % mapsize.first;
                 coordinate.second = rand() % mapsize.second;
             }
-            cout << coordinate.first << coordinate.second;
+            return coordinate;
         }
 };
 
@@ -53,14 +35,33 @@ class Game{
         Robot *robot;
         Kecoa *kecoa;
         pair<int,int> size;
+        void init(Robot *input_robot,Kecoa *input_kecoa){
+            cout << 
+            "====================" << endl <<   
+            "Welcome to the Game!" << endl << 
+            "====================" << endl;
+            int size_x,size_y,senjata;
+            cout << "Masukkan besar map:\n1. Lebar: "; cin >> size_x;
+            cout << "2. Tinggi: "; cin >> size_y; 
+            size = make_pair(size_x,size_y);
+            cout << "Masukkan panjang senjata: "; cin >> senjata;
+            gerak("help");
+            robot = &(*input_robot);
+            kecoa = &(*input_kecoa);
+            (*kecoa).init(size);
+            (*robot).init(senjata);
+            showMap();
+        }
         void showMap(){
-            for(int i = size.first-1;i>-1;i--){
-                for(int j = 0;j<size.second;j++){
+            for(int i = size.second;i>=-1;i--){
+                for(int j = -1;j<=size.first;j++){
                     if(j == (*robot).coordinate.first && i == (*robot).coordinate.second){
                         cout << "# ";
                         continue;
                     }else if(j == (*kecoa).coordinate.first && i == (*kecoa).coordinate.second){
                         cout << "X ";
+                    } else if(j == size.first || i == size.second || i == -1 || j==-1){
+                        cout << "O ";
                     } else{
                         cout << "_ ";
                     }
@@ -69,12 +70,62 @@ class Game{
             }
             cout << endl;
         }
-        void init(Robot *input_robot,Kecoa *input_kecoa,pair<int,int> mapsize){
-            robot = &(*input_robot);
-            kecoa = &(*input_kecoa);
-            size = mapsize;
-            (*robot).init(size);
-            (*kecoa).init(size);
+        string gerak(string gerak){
+            int *x = &((*robot).coordinate.first);
+            int *y = &((*robot).coordinate.second);
+            if(gerak == "kanan"){
+                if(
+                    *x == size.first-1 
+                    || 
+                        (*x == (*kecoa).coordinate.first-1
+                        &&
+                        *y == (*kecoa).coordinate.second) 
+                ) return "Menabrak sesuatu\n";
+                *x += 1; 
+            } else if(gerak == "kiri") {
+                if(
+                    *x == 0 
+                    || 
+                        (*x == (*kecoa).coordinate.first+1
+                        &&
+                        *y == (*kecoa).coordinate.second)
+                ) return "Menabrak sesuatu\n";
+                *x -= 1;
+            } else if(gerak=="atas") {
+                if(
+                    *y == size.second-1 
+                    || 
+                        (*y == (*kecoa).coordinate.second-1
+                        &&
+                        *x == (*kecoa).coordinate.first
+                        )
+            ) return "Menabrak sesuatu\n";
+                *y += 1;
+            } else if(gerak=="bawah"){
+                if(*y == 0 || (*y == (*kecoa).coordinate.second+1 && *x == (*kecoa).coordinate.first)) return "Menabrak sesuatu\n";
+                *y -= 1;
+            } else if(gerak=="serang") {
+                long long jarak = sqrt(pow((*robot).coordinate.first - (*kecoa).coordinate.first,2) + pow((*robot).coordinate.second - (*kecoa).coordinate.second,2));
+                if(jarak <= (*robot).panjang_senjata){
+                    return "win";
+                } else {
+                    return "jarak";
+                }
+            } else if(gerak=="help") {
+                cout << 
+                "Commands : "          << endl <<
+                "1. type kanan to move right" << endl <<
+                "2. type kiri to move left" << endl <<
+                "3. type atas to move up" << endl <<
+                "4. type bawah to move down" << endl << 
+                "5. type help to show all the commands list" << endl <<
+                "6. type showmap to show the map if necessary" << endl;
+            } else if(gerak == "showmap"){
+                showMap();
+            } else {
+                return "Input tidak valid!\n";
+            }
+            return "";
         }
 };
 
@@ -83,17 +134,27 @@ int main(){
     Robot anu;
     Kecoa kecoa;
     Game game;
-    game.init(&anu,&kecoa,make_pair(10,10));
+    game.init(&anu,&kecoa);
     while(true){
         string input;
-        cout << "Pilih gerakan : ";
+        cout << "Masukkan Command : ";
         cin >> input;
-        string error = anu.gerak(input);
-        if(error != ""){
-            cout << error << endl;
+        string feedback = game.gerak(input);
+        if(feedback != ""){
+            if(feedback == "win") {
+                cout << "Game over!\nYou won!";
+                break;
+            } else if(feedback == "jarak") {
+                cout << "Senjata tidak bisa menjangkau kecoa!" << endl; 
+            } else if(feedback == "none") {
+                continue;
+            } else {
+                cout << feedback << endl;
+            }
         } else {
-             game.showMap();
+            game.showMap();
         }
-    }
-    
+    } 
 }
+
+    
